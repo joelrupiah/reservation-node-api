33bb8879a1723ad7ae2fbe20a056705fdcc1aff3
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-08-13 18:00:41 +0000

    schemas

--- models/Room.js
+++ models/Room.js
@@ -1,34 +1,30 @@
 import mongoose from 'mongoose'
 
 const RoomSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    roomPrice: {
-        type: Number,
-        required: true
-    },
-    maxPeople: {
-        type: Number,
-        required: true
-    },
-    desc: {
-        type: String,
-        required: true
-    },
-    roomNumbers: [
-        {
-            number: Number,
-            unavailableDates: {
-                type: [Date]
-            }
-        }
-    ],
-},
-    {
-        timestamps: true
+  title: {
+    type: String,
+    required: true
+  },
+  roomPrice: {
+    type: Number,
+    required: true
+  },
+  maxPeople: {
+    type: Number,
+    required: true
+  },
+  desc: {
+    type: String,
+    required: true
+  },
+  roomNumbers: [{
+    number: Number,
+    unavailableDates: {
+      type: [Date]
     }
-)
+  }],
+}, {
+  timestamps: true
+})
 
 export default mongoose.model('Room', RoomSchema)
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,27 @@
 import express from 'express'
+import { createRoom, updateRoom, deleteRoom, getRoom, getRooms } from '../contollers/room.js'
+import { verifyToken, verifyAdmin } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('rooms router test')
-})
+// CREATE ROOMS
+
+router.post('/:hotelid', verifyAdmin, createRoom)
+
+// UPDATE ROOMS
+
+router.put('/:id', verifyAdmin, updateRoom)
+
+// DELETE ROOMS
+
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom)
+
+// GET SPECIFIC ROOM
+
+router.get('/:id', getRoom)
+
+// GET ALL ROOMS
+
+router.get('/', getRooms)
 
 export default router
